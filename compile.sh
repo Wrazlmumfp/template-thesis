3f35239f5e1c7a12a1219d4d76df348e11845857
Wrazlmumfp <bernhard.andraschko@gmx.de>
2021-02-11 15:46:07 +0100

    changed some stuff

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #pdflatex -output-directory="temp" $1
-filename=$(basename -- "$1")
+
+filename=$(basename -- "${1:-main.tex}")
 filename="${filename%.*}"
-pdflatex -output-directory="temp" $1
-pdflatex -output-directory="temp" $1
-mv temp/$filename.pdf .
+pdflatex -output-directory="temp" ${1:-main.tex}
+pdflatex -output-directory="temp" ${1:-main.tex}
+mv temp/$filename.pdf thesis.pdf
