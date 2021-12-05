06d91fced67ccac80d25706cdd3f7c2ab0ac3578
Wrazlmumfp <bernhard.andraschko@gmx.de>
2021-12-05 11:15:50 +0100

    fixed bibliography

--- compile.sh
+++ compile.sh
@@ -3,5 +3,6 @@
 filename=$(basename -- "${1:-main.tex}")
 filename="${filename%.*}"
 pdflatex -output-directory="temp" ${1:-main.tex}
+biber temp/$filename
 pdflatex -output-directory="temp" ${1:-main.tex}
 mv temp/$filename.pdf thesis.pdf
